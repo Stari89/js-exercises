c607d56ee83ce5bbe82f41897a044f91f987e01b
Damjan Kovačič <letecnez@gmail.com>
2025-01-12 12:54:17 +0100

    New project 27

---
+++ 27-planets-test/webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const WorkerUrlPlugin = require('worker-url/plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        // for shared array buffer
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+        },
+    },
+    performance: {
+        maxAssetSize: 10485760,
+        maxEntrypointSize: 1048576,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif|glb)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '26 Shared Game State',
+            meta: { viewport: 'width=device-width, user-scalable=no' },
+        }),
+        new CleanWebpackPlugin(),
+        new WorkerUrlPlugin(),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            os: false,
+            child_process: false,
+            worker_threads: false,
+        },
+    },
+    watch: true,
+};
